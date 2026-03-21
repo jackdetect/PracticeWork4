e0364674b5f5436e108dbaedbcb2f6ea49b4ace5
jackdetect <adanilov59@gmail.com>
2026-03-21 14:34:35 +0300

    Initial commit: project structure

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String inputFile  = "input.txt";
+        String outputFile = "output.txt";
+        System.out.println("Программа запущена");
+        System.out.println("Входной файл: " + inputFile);
+        System.out.println("Выходной файл: " + outputFile);
+    }
+}
